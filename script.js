becb12cb839a3a897133fe1a8034e126b43ff263
Aisha Yusuff <aishatyusuffe@gmail.com>
2022-12-07 14:11:06 +0000

    Adds functionality

---
+++ script.js
@@ -0,0 +1,47 @@
+const progressLine = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1; 
+
+/* Next button */
+next.addEventListener('click', () => {
+    currentActive ++;
+    if(currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
+})
+ /*Prev button */
+prev.addEventListener('click', () => {
+    currentActive--
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+    update();
+})
+
+/* Update function */
+function update() {
+    circles.forEach((circle, index) => {
+        if(index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+    progressLine.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+/*Prev and Next buttons disabled */
+    if(currentActive ===1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
